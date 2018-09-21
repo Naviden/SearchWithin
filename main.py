de0e6c925be98ec6a0275c90b22a76594addfd41
Navid Nobani <navid.nobani@gmail.com>
2018-09-21 18:47:59 +0200

    Add glob file loop

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 # import packages
 import PyPDF2
 import re
+import glob
 
-# open the pdf file
-object = PyPDF2.PdfFileReader("ISLR.pdf")
+path = '/Users/Navid/Documents/books'
+extention = 'pdf'
 
-# get number of pages
-NumPages = object.getNumPages()
+for file in glob.glob(path + '/*.' + extention):
+    print(file)
 
-# define keyterms
-String = "package"
 
-# extract text and do the search
-for i in range(0, NumPages):
-    PageObj = object.getPage(i)
-    Text = PageObj.extractText()
-    ResSearch = re.search(String, Text)
-    if ResSearch is not None:
-        print(i + 1)
+
+# # open the pdf file
+# object = PyPDF2.PdfFileReader("ISLR.pdf")
+
+# # get number of pages
+# NumPages = object.getNumPages()
+
+# # define keyterms
+# String = "package"
+
+# # extract text and do the search
+# for i in range(0, NumPages):
+#     PageObj = object.getPage(i)
+#     Text = PageObj.extractText()
+#     ResSearch = re.search(String, Text)
+#     if ResSearch is not None:
+#         print(i + 1)
