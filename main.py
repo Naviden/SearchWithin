c7a536b57754a57766e7bafc1b8cebd379c4b8d4
Navid Nobani <navid.nobani@gmail.com>
2018-09-21 19:05:42 +0200

    Add try except to text extraction loop

--- main.py
+++ main.py
@@ -3,27 +3,29 @@ import PyPDF2
 import re
 import glob
 
+
 path = '/Users/Navid/Documents/books'
 extention = 'pdf'
+query = 'python'
 
-for file in glob.glob(path + '/*.' + extention):
-    print(file)
-
-
+files = glob.glob(path + '/*.' + extention)
+file = files[0]   # print(file)
+print(file)
 
 # # open the pdf file
-# object = PyPDF2.PdfFileReader("ISLR.pdf")
+object = PyPDF2.PdfFileReader(file, strict=False)
 
 # # get number of pages
-# NumPages = object.getNumPages()
+NumPages = object.getNumPages()
 
-# # define keyterms
-# String = "package"
 
-# # extract text and do the search
-# for i in range(0, NumPages):
-#     PageObj = object.getPage(i)
-#     Text = PageObj.extractText()
-#     ResSearch = re.search(String, Text)
-#     if ResSearch is not None:
-#         print(i + 1)
+# extract text and do the search
+for i in range(0, NumPages):
+    PageObj = object.getPage(i)
+    try:
+        Text = PageObj.extractText()
+        ResSearch = re.search(query, Text)
+        if ResSearch is not None:
+            print(i + 1)
+    except:
+        print('error')
