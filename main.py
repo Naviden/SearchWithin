cc24be47be85eb106a3f95a57fc14eb868115551
Navid Nobani <navid.nobani@gmail.com>
2018-09-22 09:13:09 +0200

    Add basic statistics for a single book

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import glob
 
 path = '/Users/Navid/Documents/books'
 extention = 'pdf'
-query = 'python'
+query = 'median'
 
 files = glob.glob(path + '/*.' + extention)
 file = files[0]   # print(file)
-print(file)
+# print(file)
 
 # # open the pdf file
 object = PyPDF2.PdfFileReader(file, strict=False)
@@ -18,14 +18,23 @@ object = PyPDF2.PdfFileReader(file, strict=False)
 # # get number of pages
 NumPages = object.getNumPages()
 
-
 # extract text and do the search
+errs = 0
+fon = 0
 for i in range(0, NumPages):
     PageObj = object.getPage(i)
     try:
         Text = PageObj.extractText()
         ResSearch = re.search(query, Text)
         if ResSearch is not None:
-            print(i + 1)
+
+            fon += 1
     except:
-        print('error')
+        errs += 1
+
+un = round(((errs / NumPages) * 100), 1)
+
+title = file.split('/')[-1].split('.')[0].replace('-',' ')
+print(f'Title : {title}')
+print(f'Unreachable : {un}%')
+print(f'Found : {fon}')
