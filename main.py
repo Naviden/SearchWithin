df13285f273dac57287d3631440df0137c5574a9
Navid Nobani <navid.nobani@gmail.com>
2018-09-22 09:16:13 +0200

    Add loop to the code

--- main.py
+++ main.py
@@ -9,32 +9,33 @@ extention = 'pdf'
 query = 'median'
 
 files = glob.glob(path + '/*.' + extention)
-file = files[0]   # print(file)
-# print(file)
+for file in files:
+    # print(file)
 
-# # open the pdf file
-object = PyPDF2.PdfFileReader(file, strict=False)
+    # # open the pdf file
+    object = PyPDF2.PdfFileReader(file, strict=False)
 
-# # get number of pages
-NumPages = object.getNumPages()
+    # # get number of pages
+    NumPages = object.getNumPages()
 
-# extract text and do the search
-errs = 0
-fon = 0
-for i in range(0, NumPages):
-    PageObj = object.getPage(i)
-    try:
-        Text = PageObj.extractText()
-        ResSearch = re.search(query, Text)
-        if ResSearch is not None:
+    # extract text and do the search
+    errs = 0
+    fon = 0
+    for i in range(0, NumPages):
+        PageObj = object.getPage(i)
+        try:
+            Text = PageObj.extractText()
+            ResSearch = re.search(query, Text)
+            if ResSearch is not None:
+                fon += 1
+        except:
+            errs += 1
 
-            fon += 1
-    except:
-        errs += 1
+    un = round(((1 - (errs / NumPages)) * 100), 1)
 
-un = round(((errs / NumPages) * 100), 1)
-
-title = file.split('/')[-1].split('.')[0].replace('-',' ')
-print(f'Title : {title}')
-print(f'Unreachable : {un}%')
-print(f'Found : {fon}')
+    title = file.split('/')[-1].split('.')[0].replace('-', ' ')
+    print(f'Title : {title}')
+    print(f'Reachable : {un}%')
+    print(f'Found : {fon}')
+    print('**********************')
+    print()
